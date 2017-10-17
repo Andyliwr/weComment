4147e7d0c727637732870058e365209e42dab783
andyliwr <andyliwr@outlook.com>
2017-10-18 00:10:20 +0800

    修复认证失败的bug

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,8 +1,7 @@
-let dbName = "weComment"
-let dbHost = "mongodb://lidikang:123456@localhost:27017/wecomment"
+let dbUrl = "mongodb://admin:123456@ds036577.mlab.com:36577/we_comment"
 let mongoose = require("mongoose")
 exports.connect = function (request, response) {
-  mongoose.connect(dbHost + dbName, { useMongoClient: true }) // useMongoClient防止报错
+  mongoose.connect(dbUrl, { useMongoClient: true }) // useMongoClient防止报错
   let db = mongoose.connection
   db.on('error', console.error.bind(console, 'Database connected error:'))
   db.once('open', function (callback) {
